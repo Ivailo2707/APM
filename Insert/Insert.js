9b1764ae71ed42281b29c7d0053d9f690645b998
Ivailo2707 <Ivailo_27@abv.bg>
2023-03-09 13:12:02 +0200

    Работя над проверка за въвеждане на текст

---
+++ Insert/Insert.js
@@ -0,0 +1,8 @@
+function Submit(){
+    let Google = document.getElementById("google").value;
+    let google_str = Google.toString();
+    let Password = document.getElementById("password").value;
+    let password_str = Password.toString();
+    console.log(google_str)
+    console.log(password_str)
+}
