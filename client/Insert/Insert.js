91fcfea5c9b52d6b27bd54f16702a953a35b897b
Ivailo2707 <Ivailo_27@abv.bg>
2023-03-10 21:16:51 +0200

    Connect

--- client/Insert/Insert.js
+++ client/Insert/Insert.js
@@ -1,94 +1,29 @@
-
-
-(function() {
-  'use strict';
-
-  hterm.defaultStorage = new lib.Storage.Local();
-
-  var port;
-
-  let textEncoder = new TextEncoder();
-
-  let t = new hterm.Terminal();
-  t.onTerminalReady = () => {
-    console.log('Terminal ready.');
-    let io = t.io.push();
-
-    io.onVTKeystroke = str => {
-      if (port !== undefined) {
-        port.send(textEncoder.encode(str)).catch(error => {
-          t.io.println('Send error: ' + error);
-        });
-      }
-    };
-
-    io.sendString = str => {
-      if (port !== undefined) {
-        port.send(textEncoder.encode(str)).catch(error => {
-          t.io.println('Send error: ' + error);
-        });
-      }
-    };
-  };
-
-  document.addEventListener('DOMContentLoaded', event => {
-    let connectButton = document.querySelector('#connect');
-    t.decorate(document.querySelector('#terminal'));
-    t.setWidth(80);
-    t.setHeight(24);
-    t.installKeyboard();
-
-    function connect() {
-      t.io.println('Connecting to ' + port.device_.productName + '...');
-      port.connect().then(() => {
-        console.log(port);
-        t.io.println('Connected.');
-        connectButton.textContent = 'Disconnect';
-        port.onReceive = data => {
-          let textDecoder = new TextDecoder();
-          t.io.print(textDecoder.decode(data));
-        }
-        port.onReceiveError = error => {
-          t.io.println('Receive error: ' + error);
-        };
-      }, error => {
-        t.io.println('Connection error: ' + error);
-      });
-    };
-
-    connectButton.addEventListener('click', function() {
-      if (port) {
-        port.disconnect();
-        connectButton.textContent = 'Connect';
-        port = null;
-      } else {
-        serial.requestPort().then(selectedPort => {
-          port = selectedPort;
-          connect();
-        }).catch(error => {
-          t.io.println('Connection error: ' + error);
-        });
-      }
-    });
-
-    serial.getPorts().then(ports => {
-      if (ports.length == 0) {
-        t.io.println('No devices found.');
-      } else {
-        port = ports[0];
-        connect();
-      }
-    });
-  });
-})();
-
 function submit_data(){
-    let password = document.getElementById("password").value;
-    let site_name = document.getElementById("site").value;
-    document.getElementById("error").innerHTML = "";
-    if(!password){
-        document.getElementById("error").innerHTML = "Error no password given";
-    }else if(!site_name){
-        document.getElementById("error").innerHTML = "Error no site given"
-    }
+  let password = document.getElementById("password").value;
+  let site_name = document.getElementById("site").value;
+  document.getElementById("error").innerHTML = "";
+  if(!password){
+      document.getElementById("error").innerHTML = "Error no password given";
+  }else if(!site_name){
+      document.getElementById("error").innerHTML = "Error no site given"
+  }
 }
+
+function connect1(){
+let arduino;
+navigator.usb.requestDevice({ filters: [{ vendorId: 0x2341 }] })
+.then(device => {
+arduino = device
+console.log(device);
+return arduino.open()
+})
+// .then(() => arduino.selectConfiguration(1)) // Select configuration #1 for the device.
+.then(() => arduino.claimInterface(0)) // Request exclusive control over interface #2.
+.then((a) => {
+console.log(a);
+console.log(arduino);
+const data = new Uint8Array([1]);
+arduino.transferOut(2, data).then(t => console.log(t));
+})
+.catch(error => { console.error(error); });
+} 
