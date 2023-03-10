eebe8a777b5d4529964efe30a8725071ccd389b2
Gabi <86157167+GabrielPetrov@users.noreply.github.com>
2023-03-10 11:31:24 +0200

    added image and made the navbar hide

--- Homepage/Homepage.js
+++ Homepage/Homepage.js
@@ -1,4 +1,17 @@
 function scrollDown() {
     const marketing = document.querySelector("#marketing_section");
     marketing.scrollIntoView({ behavior: "smooth" });
+}
+
+var lastY = window.pageYOffset;
+
+function scroll_navbar() {
+  var currY = window.pageYOffset;
+  if (lastY > currY) {
+    document.getElementById("navbar").style.top = "0px";
   }
+  else {
+    document.getElementById("navbar").style.top = "-4rem";
+  }
+  lastY = currY;
+}
